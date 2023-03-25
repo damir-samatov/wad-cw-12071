ab05320b4c8392d8de6abc7059b82ad802498af7
Damir <damirsamatofff@gmail.com>
2023-03-26 02:20:37 +0500

    Init

---
+++ wad-cw-12071-backend/wad-cw-12071-backend/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace wad_cw_12071_backend
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + ( int )( TemperatureC / 0.5556 );
+
+        public string? Summary { get; set; }
+    }
+}
